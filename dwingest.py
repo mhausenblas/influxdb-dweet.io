d6cb759f4bbdeb3ca804f5a245782085dd6187c0
Michael Hausenblas <michael.hausenblas@gmail.com>
2014-09-07 07:17:10 +0100

    adds polling

--- dwingest.py
+++ dwingest.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 """
-Allows to ingest data from dweet.io into InfluxDB.
+Allows to ingest data from dweet.io into InfluxDB. You must supply the InfluxDB
+hostname, the thing (cf. https://dweet.io/see)) and the thing's key, that is,
+a key in the JSON map representation of the thing. You can supply a poll interval,
+and if you don't supply one 10 sec will be used as default.
 
 Usage: 
 
-  `python dwingest.py influxdb AvocadoGrove aiHotWaterTemp_degreesF`  ... run using host `influxdb` to store TS
+   To run using host `influxdb` to ingest a data point of thing `AvocadoGrove` 
+   with key `aiHotWaterTemp_degreesF` every 10 sec (that is, the default):
+     
+  `python dwingest.py influxdb AvocadoGrove aiHotWaterTemp_degreesF`
 
 @author: Michael Hausenblas, http://mhausenblas.info/#i
 @since: 2014-09-06
@@ -12,7 +18,7 @@ Usage:
 """
 
 import sys
-import os
+import time
 import logging
 import urllib2
 import json
@@ -39,6 +45,8 @@ DATABASE =  "dweet"
 
 DWEETIO_BASE = "https://dweet.io:443/get/latest/dweet/for/"
 
+DEFAULT_WAIT = 10 # that is, wait per default for 10 sec between trying to query
+                  # dweet.io and push a new data point into InfluxDB
 
 def parse_value(the_thing, the_key):
   try:
@@ -52,38 +60,54 @@ def parse_value(the_thing, the_key):
     sys.exit(2)
 
 def push_datapoint(ifx_client, the_thing, the_key):
-    dvalue = parse_value(the_thing, the_key)
-    datapoint = [{
-            "name": DATABASE,
-            "columns": ["thing", the_key],
-            "points": [
-                [the_thing, dvalue]
-            ]
-    }]
-    logging.info("Ingesting {0}".format(datapoint))
-    ifx_client.write_points(datapoint)
+  dvalue = parse_value(the_thing, the_key)
+  datapoint = [{
+          "name": DATABASE,
+          "columns": ["thing", the_key],
+          "points": [
+              [the_thing, dvalue]
+          ]
+  }]
+  logging.debug("Ingesting {0}".format(datapoint))
+  ifx_client.write_points(datapoint)
+  return dvalue
+
+def ingest(host, thing, key, poll_wait):
+  ifx_client = influxdb.InfluxDBClient(host, PORT, USER_NAME, USER_PWD, DATABASE)
+  logging.info("="*80)
+  logging.info("Querying thing %s with key %s every %d sec" %(thing, key, poll_wait))
+  while True:
+    logging.info("-"*80)
+    dvalue = push_datapoint(ifx_client, thing, key)
+    logging.info("Ingested value: %s" %(dvalue))
+    time.sleep(poll_wait)
 
 def usage():
   print("Usage:")
-  print(" python dwingest.py $INFLUXDB_HOSTNAME $THING $KEY")
+  print(" python dwingest.py $INFLUXDB_HOSTNAME $THING $KEY $POLL_INTERVAL")
   print("\nExample:")
   print(" python dwingest.py influxdb AvocadoGrove aiHotWaterTemp_degreesF")
+  print(" python dwingest.py influxdb AvocadoGrove aiHotWaterTemp_degreesF 20")
 
 ################################################################################
 # main script
 #
 if __name__ == '__main__':
   try:
-      if sys.argv[1]:
-        host = sys.argv[1]
-      if sys.argv[2]:
-        thing = sys.argv[2]
-      if sys.argv[3]:
-        key = sys.argv[3]
-      
+      if len(sys.argv) < 4:          # we don't know the DB, thing and key ...
+        usage()                      # ... show how to use it and ...
+        sys.exit(2)                  # ... don't even think about trying
+      elif len(sys.argv) == 4:       # we know all but the poll interval ...
+        poll_wait = DEFAULT_WAIT     # ... use default poll interval
+      else:                          #
+        poll_wait = int(sys.argv[4]) # parse and use supplied poll interval
+
+      host = sys.argv[1]
+      thing = sys.argv[2]
+      key = sys.argv[3]      
       logging.info("Using InfluxDB host at %s:%s" %(host, PORT))
-      ifx_client = influxdb.InfluxDBClient(host, PORT, USER_NAME, USER_PWD, DATABASE)
-      push_datapoint(ifx_client, thing, key)
+      ingest(host, thing, key, poll_wait)
   except Exception, e:
+    logging.error(e)
     usage()
     sys.exit(2)
